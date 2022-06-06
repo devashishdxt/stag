a64c0d3bb71c4f321ca4a3f7f2a67f039a0328ce
Devashish Dixit <devashishdxt@gmail.com>
2022-06-06 14:19:47 +0800

    fix sidebar size

--- stag-ui/src/componenets/sidebar.rs
+++ stag-ui/src/componenets/sidebar.rs
@@ -3,14 +3,7 @@ use yew_router::{hooks::use_route, prelude::Link};
 
 use crate::routes::Route;
 
-const CONTAINER_CLASSES: &[&str] = &[
-    "fixed",
-    "w-60",
-    "bg-slate-900",
-    "h-screen",
-    "py-6",
-    "text-slate-50",
-];
+const CONTAINER_CLASSES: &[&str] = &["w-60", "min-w-max", "bg-slate-900", "py-6", "text-slate-50"];
 
 const LINK_CLASSES: &[&str] = &[
     "p-4",
@@ -25,7 +18,7 @@ pub fn sidebar() -> Html {
     let current_route: Route = use_route().unwrap_or_default();
 
     html! {
-        <div class={classes!(CONTAINER_CLASSES)}>
+        <div class={classes!(CONTAINER_CLASSES, "min-h-screen")}>
             <Link<Route> to={Route::Home}>
                 <div class={classes!(LINK_CLASSES, (current_route == Route::Home).then(|| ["border-r-4", "bg-slate-800"].as_ref()))}>
                     <i class={classes!("fa-solid", "fa-house", "w-8", "text-center", "mr-4")}></i><span>{ "Home" }</span>
@@ -52,7 +45,7 @@ pub fn sidebar() -> Html {
                     <i class={classes!("fa-solid", "fa-bridge", "w-8", "text-center", "mr-4")}></i><span>{ "Channels" }</span>
                 </div>
             </Link<Route>>
-            <fieldset class={classes!("border-t-2", "border-slate-700", "border-dotted", "my-2")}><legend class={classes!("text-sm", "text-slate-400", "mx-4", "px-2")}>{ "Applications" }</legend></fieldset>
+            <fieldset class={classes!("border-t-2", "border-slate-700", "border-dotted", "my-2")}><legend class={classes!("text-sm", "text-slate-400", "mx-4", "px-2")}>{ "Transfer" }</legend></fieldset>
             <Link<Route> to={Route::Mint}>
                 <div class={classes!(LINK_CLASSES, (current_route == Route::Mint).then(|| ["border-r-4", "bg-slate-800"].as_ref()))}>
                     <i class={classes!("fa-solid", "fa-coins", "w-8", "text-center", "mr-4")}></i><span>{ "Mint" }</span>
@@ -63,6 +56,7 @@ pub fn sidebar() -> Html {
                     <i class={classes!("fa-solid", "fa-fire", "w-8", "text-center", "mr-4")}></i><span>{ "Burn" }</span>
                 </div>
             </Link<Route>>
+            <fieldset class={classes!("border-t-2", "border-slate-700", "border-dotted", "my-2")}><legend class={classes!("text-sm", "text-slate-400", "mx-4", "px-2")}>{ "Interchain Accounts" }</legend></fieldset>
             <Link<Route> to={Route::Ica}>
                 <div class={classes!(LINK_CLASSES, (current_route == Route::Ica).then(|| ["border-r-4", "bg-slate-800"].as_ref()))}>
                     <i class={classes!("fa-solid", "fa-user-astronaut", "w-8", "text-center", "mr-4")}></i><span>{ "ICA" }</span>
